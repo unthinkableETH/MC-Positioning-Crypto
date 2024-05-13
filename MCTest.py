8f355d51322fb9844afe6b50158f52b3c3517367
Shaun <158432022+unthinkableETH@users.noreply.github.com>
2024-05-12 19:59:49 -0700

    Update MCTest.py

--- MCTest.py
+++ MCTest.py
@@ -20,9 +20,9 @@ if d1!=None and d2!=None:
         df1=pd.read_parquet(s3_1)
         df2=pd.read_parquet(s3_2)
         df3=pd.merge(df1, df2, on='symbol')
-        df3["Difference"]=(df3['Rank_y'])-(df3['Rank_x'])
-        df3=df3.drop(['display_symbol_x','aliases_x','shortname_x','last_price_usd_x','volume_24_usd_x','market_cap_usd_x','Rank_y','display_symbol_y'], axis=1)
-        df3=df3.drop(['name_y','aliases_y','shortname_y','last_price_usd_y','volume_24_usd_y','market_cap_usd_y'], axis=1)
+        df3["Difference"]=(df3['Rank_x'])-(df3['Rank_y'])
+        df3=df3.drop(['display_symbol_x','aliases_x','shortname_x','last_price_usd_x','volume_24_usd_x','market_cap_usd_x','Rank_x','display_symbol_y'], axis=1)
+        df3=df3.drop(['name_x','aliases_y','shortname_y','last_price_usd_y','volume_24_usd_y','market_cap_usd_y'], axis=1)
         st.dataframe(df3,hide_index=True)
     except FileNotFoundError:
         st.header(' Error. Reminder only the first of each month can be selected. Please select a different date.')
